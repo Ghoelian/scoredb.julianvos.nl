242694c9f5bd95c4ab1cc30afacd9c471c2d2f3f
Julian Vos <post@julianvos.nl>
2019-07-03 13:17:17 +0200

    Fixed log_out(), forgot to specify the domain for cookies on logout.

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 require_once(__DIR__ . '/include/Login.php');
 require_once(__DIR__ . '/include/Header.php');
 require_once(__DIR__ . '/include/Head.php');
-require_once(__DIR__ . '/include/Database.php');
 
 $Head = new Head();
 $Header = new Header();
 $Login = new Login();
-$Database = new Database();
 ?>
 
 <html>
@@ -22,20 +20,31 @@ $Database = new Database();
 <body>
     <?php
 $Header->echoHeader();
-    if (isset($_COOKIE['username']) && isset($_COOKIE['token']) && $Login->check($_COOKIE['username'], $_COOKIE['token'])) {
-        echo "<p><a href='/add'>Add an fc</a></p>";
-        // echo "<p><a href='/edit'>Edit an fc</a></p>";
-        echo "<p><a href='/view'>View a user's scores</a></p>";
-        echo "<p><a href='/login/logout.php'>Log out</a></p>";
-    } else {
-        echo "
-    <div class='menu'>
+
+echo "<div class='menu'>";
+
+    if (isset($_COOKIE['username']) && isset($_COOKIE['token'])) {
+        if ($Login->check($_COOKIE['username'], $_COOKIE['token'])) {
+            echo "<p><a href='/add'>Add an fc</a></p>";
+            // echo "<p><a href='/edit'>Edit an fc</a></p>";
+            echo "<p><a href='/view'>View a user's scores</a></p>";
+            echo "<p><a href='/login/logout.php'>Log out</a></p>";
+        } else {
+            echo "
         <p><a href='/login'>Log in</a></p>
         <p><a href='/register'>Sign up</a></p>
         <p><a href='/view'>View a user's scores</a></p>
-    </div>
+";
+        }
+    } else {
+        echo "
+        <p><a href='/login'>Log in</a></p>
+        <p><a href='/register'>Sign up</a></p>
+        <p><a href='/view'>View a user's scores</a></p>
 ";
     }
+
+    echo "</div>";
     
 ?>
 </body>
