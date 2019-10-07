38f030769ef3d7fb6f7fda74555004d74ffff78a
Julian <post@julianvos.nl>
2019-10-07 20:43:01 +0200

    Created dedicated page for viewing image of score

---
+++ viewImage/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once(__DIR__ . '/../include/Login.php');
+require_once(__DIR__ . '/../include/Header.php');
+require_once(__DIR__ . '/../include/Head.php');
+
+$Head = new Head();
+$Header = new Header();
+$Login = new Login();
+
+if (!isset($_GET['image'])) {
+	header("Location: /view");
+}
+?>
+
+<html>
+
+<head>
+	<title>Score database</title>
+	<?php
+	$Head->echoHead();
+	?>
+</head>
+
+<body>
+	<?php
+	$Header->echoHeader();
+	?>
+
+	<img src=' <?php
+	echo $_GET['image']
+	?>'>
+</body>
+
+</html>
